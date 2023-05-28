ccca905bd802533a00d364c74d1058dc98e72df4
elkasztano <112324044+elkasztano@users.noreply.github.com>
2023-05-28 14:34:27 +0200

    Add files via upload

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,3 @@
-// GPIO-Pin the 433 Mhz TX module is connected to
 pub const RC_PIN: u8 = 17;
 
 // binary sequence for 'Brennenstuhl RCS 1000SN' 433 Mhz remote switch
@@ -9,7 +8,7 @@ pub const RC_PIN: u8 = 17;
 // pairing: the remote and the sockets have dip switches that need to match
 // first 10 bits: '00' if dip switch is up, '01' if dip switch is down, 5 dip switches alltogether
 // change this according to your requirements
-pub const DIP_SWITCH: &str = "s0100000001"; // example: first and last dip switch down, all others up
+pub const DIP_SWITCH: &str = "s0000000000"; // example: first and last dip switch down, all others up
 // the 's' stands for the sync bit at the beginning of each transmission
 
 // next 10 bits: a sequence of '00' and '01' corresponding to socket switches A,B,C,D
@@ -28,23 +27,43 @@ pub const RC_OFF: &str = "0100";
 // example codes for 'PHYSEN' doorbell
 // the doorbell needs to be programmed first in order to react to the corresponding binary sequence
 // note that one device can react to multiple different codes
-pub const DOORBELL_RING: [ &str; 3 ] = [
-"s000011000010111110011010",
-"s000111010110111000001111",
-"s010101011111001101011001"
+pub const DOORBELL_RING: [ &str; 10 ] = [
+"s001100100101100101010110",
+"s001010100011110010101101",
+"s100011010110111011001111",
+"s010101010001001101011001",
+"s110110110110110100011010",
+"s010100000001010001111010",
+"s010111010111000110110000",
+"s111000111000101010001101",
+"s001100100101100101010011",
+"s100101001001001110110011",
 ];
+
+// 'Gmornxen' socket switch presets
+pub const XEN_PRE: &str = "s10";
+
+pub const XEN_POST: &str = "001100100001001100000000";
+
+pub const XEN_AON: &str = "001110";
+pub const XEN_AOFF: &str = "000001";
+
+pub const XEN_BON: &str = "100110";
+pub const XEN_BOFF: &str = "101110";
+
+pub const XEN_CON: &str = "010110";
+pub const XEN_COFF: &str = "011110";
 
 // defining pulse length ('PL', microseconds) and repeats ('RP')
 
 // finding the right pulse length usually requires a lot of fine tuning and may be a bit tricky
 // the right pulse length for the Brennenstuhl RCS 1000SN seems to be 320 microseconds, and 190
 // microseconds for the Physen doorbells respectively
-pub const RC_PL: u16 = 310;
-pub const DB_PL: u16 = 179;
+pub const RC_PL: u16 = 320;
+pub const DB_PL: u16 = 175;
 
 // the number of repeats should be as low as reasonably achievable
 // if it is too long other 433 Mhz devices like weather stations or even tire pressure control
 // systems may get blocked
 pub const RC_RP: u8 = 10;
 pub const DB_RP: u8 = 5;
-
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use rppal::gpio::Gpio;
 use clap::{Parser, Subcommand, ValueEnum};
 use std::process::ExitCode;
 use sparkypi::constants::*;
-use sparkypi::Transmission;
+use sparkypi::*;
 
 
 #[derive(Parser, Debug)]
@@ -25,7 +25,16 @@ enum Commands {
         #[arg(short, long = "state", value_enum)]
         state: State,
     },
-    
+    /// control mains socket 'gmornxen'
+    Xen {
+        /// device
+        #[arg(short, long = "device", value_enum)]
+        device: XenDevice,
+
+        /// state
+        #[arg(short, long = "state", value_enum)]
+        state: State,
+    },
     /// trigger doorbell
     Bell {
         /// device id
@@ -44,7 +53,17 @@ enum Commands {
         /// number of repeats
         #[arg(short, long = "repeats", value_name = "U8")]
         rp: u8,
+        /// protocol
+        #[arg(long = "protocol", value_name = "PROTOCOL")]
+        protocol: ProtocolChoice,
     },
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum ProtocolChoice {
+    Protocol1,
+    Protocol2,
+    Xen,
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
@@ -53,6 +72,13 @@ enum Device {
     B,
     C,
     D,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum XenDevice {
+    A,
+    B,
+    C,
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
@@ -70,26 +96,23 @@ fn main() -> ExitCode {
     
     let args = Args::parse();
 
-    let mut tm = Transmission {
-        sequence: String::new(),
-        pulse_length: 0,
-        repeats: 0,
-    };
+    let mut tm = Transmission::new();
     
     match &args.command {
         
         Commands::Socket { device, state } => {
             
             tm.sequence.push_str(DIP_SWITCH);
-            tm.pulse_length = RC_PL;
-            tm.repeats = RC_RP;
+            tm.pulse_length = 310;
+            tm.repeats = 10;
+            tm.protocol = P1; 
 
             match device {
                 Device::A => tm.sequence.push_str(RC_SWITCH[0]),
                 Device::B => tm.sequence.push_str(RC_SWITCH[1]),
                 Device::C => tm.sequence.push_str(RC_SWITCH[2]),
                 Device::D => tm.sequence.push_str(RC_SWITCH[3]),
-            }
+           }
 
             match state {
                 State::On => tm.sequence.push_str(RC_ON),
@@ -98,10 +121,37 @@ fn main() -> ExitCode {
         
         },
 
+        Commands::Xen { device, state } => {
+
+            tm.sequence.push_str(XEN_PRE);
+            tm.pulse_length = 580;
+            tm.repeats = 10;
+            tm.protocol = XEN;
+
+            match device {
+                XenDevice::A => match state {
+                    State::On => tm.sequence.push_str(XEN_AON),
+                    State::Off => tm.sequence.push_str(XEN_AOFF),
+                },
+                XenDevice::B => match state {
+                    State::On => tm.sequence.push_str(XEN_BON),
+                    State::Off => tm.sequence.push_str(XEN_BOFF),
+                },
+                XenDevice::C => match state {
+                    State::On => tm.sequence.push_str(XEN_CON),
+                    State::Off => tm.sequence.push_str(XEN_COFF),
+                },
+            }
+
+            tm.sequence.push_str(XEN_POST);
+
+        },
+
         Commands::Bell { device } => {
 
-            tm.pulse_length = DB_PL;
-            tm.repeats = DB_RP;
+            tm.pulse_length = 175;
+            tm.repeats = 5;
+            tm.protocol = P1;
             
             let seq = match DOORBELL_RING.get(*device as usize) {
                 Some(sequence) => sequence,
@@ -126,11 +176,17 @@ fn main() -> ExitCode {
             tm.sequence.push_str(seq);
         },
         
-        Commands::Cus {s, pl, rp} => {
+        Commands::Cus {s, pl, rp, protocol} => {
             tm.sequence.push_str(s);
             tm.pulse_length = *pl;
             tm.repeats = *rp;
-        },
+
+            match protocol {
+                ProtocolChoice::Protocol1 => tm.protocol = P1,
+                ProtocolChoice::Protocol2 => tm.protocol = P2,
+                ProtocolChoice::Xen => tm.protocol = XEN,
+            }
+        }
     
     }
 
